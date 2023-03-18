3a400af80aadaf459c5e3bfb3dad9a111ee8644b
Jakub Zehner <kubusja.pl@gmail.com>
2023-03-18 23:32:28 +0100

    add position caching to improve performance

--- Project1/src/utils/data_reader.v
+++ Project1/src/utils/data_reader.v
@@ -2,8 +2,10 @@ module utils
 
 import os
 import time
+import position { Position }
 
 pub fn read_csv(path string) !Rows {
+	mut pos_cache := map[string]Position{}
 	mut rows := Rows{
 		rows: []Row{cap: 273500}
 	}
@@ -16,7 +18,7 @@ pub fn read_csv(path string) !Rows {
 	eprintln('Csv file loading time: ${start_parsing_file_time - start_reading_file_time}')
 
 	for line in content.split('\n')[1..] {
-		rows.rows << parse_row(line)
+		rows.rows << parse_row(line, mut pos_cache)
 	}
 
 	eprintln('Csv parsing time: ${time.now() - start_parsing_file_time}')
--- Project1/src/utils/parser.v
+++ Project1/src/utils/parser.v
@@ -1,9 +1,20 @@
 module utils
 
 import simple_time
-import position
+import position { Position }
 
-fn parse_row(row_str string) Row {
+fn cached_position(lat string, lon string, mut pos_cache map[string]Position) Position {
+	cords := '${lat};${lon}'
+	if cords in pos_cache {
+		return pos_cache[cords]
+	}
+
+	pos := position.from(lat, lon)
+	pos_cache[cords] = pos
+	return pos
+}
+
+fn parse_row(row_str string, mut pos_cache map[string]Position) Row {
 	row := row_str.split(',')
 
 	line := row[3]
@@ -11,8 +22,8 @@ fn parse_row(row_str string) Row {
 	end_time := simple_time.from(row[5])
 	start_stop := row[6].to_lower()
 	end_stop := row[7].to_lower()
-	start_pos := position.from(row[8], row[9])
-	end_pos := position.from(row[10], row[11])
+	start_pos := cached_position(row[8], row[9], mut pos_cache)
+	end_pos := cached_position(row[10], row[11], mut pos_cache)
 
 	return Row{line, start_time, end_time, start_stop, end_stop, start_pos, end_pos}
 }
